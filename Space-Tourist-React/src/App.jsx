f0c8c06329c7fad5267c9330304712b29e5cedc6
CodingFunky <ryan.namingishard2@gmail.com>
2023-09-26 11:08:36 -0600

    copied some reference files over and finished cleaning up template and added a navbar component

--- Space-Tourist-React/src/App.jsx
+++ Space-Tourist-React/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+// import { useState } from "react";
+import Navbar from "./navbar.jsx";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
+      <Navbar />
       {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -14,12 +14,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div> */}
-      <h1>Vite + React</h1>
+      {/* <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-      </div>
+      </div> */}
     </>
   );
 }
---
+++ Space-Tourist-React/src/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="home">00 Home</div>
+      <div className="destination">01 Destination</div>
+      <div className="crew">02 Crew</div>
+    </div>
+  );
+}
+
+export default Navbar;
