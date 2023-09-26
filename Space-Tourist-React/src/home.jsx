155df4c3e83f0259cd56aa87da2da5820e7f9744
CodingFunky <ryan.namingishard2@gmail.com>
2023-09-26 12:12:31 -0600

    cleaned up names

--- Space-Tourist-React/src/home.jsx
+++ Space-Tourist-React/src/home.jsx
@@ -1,16 +1,16 @@
-function Content() {
+function Home() {
   return (
-    <div className="contentContainer">
-      <div className="textContent">
+    <div className="homeContainer">
+      <div className="homeLeftContent">
         {" "}
         So, you want to travel to Space Let’s face it; if you want to go to
         space, you might as well genuinely go to outer space and not hover kind
         of on the edge of it. Well sit back, and relax because we’ll give you a
         truly out of this world experience! Explore
       </div>
-      <div className="exploreLogo"></div>
+      <div className="homeRightContent"></div>
     </div>
   );
 }
 
-export default Content;
+export default Home;
