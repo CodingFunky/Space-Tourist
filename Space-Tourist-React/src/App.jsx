9153fce3395a81c5ff5c1a966292a042be816155
CodingFunky <ryan.namingishard2@gmail.com>
2023-09-27 05:24:11 -0600

    switched to browserrouter

--- Space-Tourist-React/src/App.jsx
+++ Space-Tourist-React/src/App.jsx
@@ -4,17 +4,18 @@ import Navbar from "./components/navbar/Navbar.jsx";
 import Destination from "./destination-moon.jsx";
 import "./App.css";
 import Home from "./components/home/home.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Router>
+      <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/destination" element={<Destination />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </>
   );
 }
