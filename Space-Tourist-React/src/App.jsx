f2942a80a4d8b9b14b907295cd4984841b5ff2a6
RochakDhakal <rochakdhakal@gmail.com>
2023-09-27 15:37:14 +0545

    navbar and home

--- Space-Tourist-React/src/App.jsx
+++ Space-Tourist-React/src/App.jsx
@@ -1,20 +1,18 @@
 // import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./navbar.jsx";
-import Home from "./home.jsx";
+import Navbar from "./components/navbar/Navbar.jsx";
 import Destination from "./destination-moon.jsx";
 import "./App.css";
+import Home from "./components/home/home.jsx";
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/destination" element={<Destination />} />
-        </Routes>
-      </Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<>Page Not Found</>} />
+      </Routes>
     </>
   );
 }
