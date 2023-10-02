09e9f2ac1949a1a871724c8791ff1ce6af61bf12
CodingFunky <ryan.namingishard2@gmail.com>
2023-10-02 14:06:11 -0600

    destination page and card component

---
+++ Space-Tourist-React/src/components/destinations/card.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from "react";
+
+import "./card.css";
+
+export default function Card(props) {
+  //   let badgeText;
+  //   if (props.openSpots === 0) {
+  //     badgeText = "SOLD OUT";
+  //   } else if (props.location === "Online") {
+  //     badgeText = "ONLINE";
+  //   }
+  return (
+    <div className="card">
+      <h1 className="cardName">{props.name}</h1>
+      <p className="cardDes">{props.description}</p>
+      <div className="cardFooter">
+        <div className="distance">
+          <span className="distanceTxt">AVG. DISTANCE </span>
+          {props.distance}
+        </div>
+        <div className="travel">
+          <span className="travelTxt">EST. TRAVEL TIME</span>
+          {props.travel}
+        </div>
+      </div>
+    </div>
+  );
+}
--- Space-Tourist-React/src/components/destinations/destination-moon.jsx
+++ Space-Tourist-React/src/components/destinations/destination-moon.jsx
@@ -1,14 +1,32 @@
 import moon from "../../../assets/destination/image-moon.png";
-
+import "./destination.css";
+import dataFile from "../../../data.json";
+import Card from "./card.jsx";
+const data = dataFile.destinations;
+console.log(data[0].description);
 function Destination() {
   return (
     <div className="destinationContainer">
-      <img src={moon} alt="Moon" />
-      <div className="destinationLeftContent">01 Pick Your Destination</div>
-
-      <div className="destinationRightContent">
-        <div className="destinationNav"></div>
-      </div>
+      <p className="destinationTitle">
+        <span className="bold">01</span> PICK YOUR DESTINATION
+      </p>
+      <section className="destinationHero">
+        <img src={moon} alt="Moon" className="moonPic" />
+        <div className="cardContainer">
+          <div className="dNav">
+            <p>Moon</p>
+            <p>Mars</p>
+            <p>Europa</p>
+            <p>Titan</p>
+          </div>
+          <Card
+            name={data[0].name}
+            description={data[0].description}
+            distance={data[0].distance}
+            travel={data[0].travel}
+          />
+        </div>
+      </section>
     </div>
   );
 }
